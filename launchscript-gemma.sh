3780f841144c49f7d9f9f38319a22d65c6816231
FATESAIKOU <qzsecftbhhhh@gmail.com>
2025-05-10 14:40:50 +0900

    Fix

---
+++ launchscript-gemma.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+mcphost -m ollama:gemma3:4b --config ~/testPY/mcp-demo/sample-mcp-config.json
---
+++ launchscript-qwen3.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+mcphost -m ollama:qwen3:8b --config ~/testPY/mcp-demo/sample-mcp-config.json
